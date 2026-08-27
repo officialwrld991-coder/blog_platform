aa59612c100dfe50a39890571b06bbacfc4ba73c
brwnboi <officialwrld991@gmail.com>
2026-08-27 17:55:20 +0100

    new commit

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel, create_engine
+from urllib.parse import quote_plus
+engine = create_engine(f"mysql+pymysql://user1:"
+                       f"{quote_plus('password')}"
+                       f"@localhost:3306/employee")
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from blog-platform!")
+from fastapi import FastAPI
+from models.user import User
+from database import create_db_and_tables
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    create_db_and_tables()
+    yield
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def home():
+    return "Hello World"
