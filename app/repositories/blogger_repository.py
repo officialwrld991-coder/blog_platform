5eba41663b3b6d7b17ead7500759b26e09a3e43d
QueenColly <colleteumeh0@gmail.com>
2026-09-01 12:15:49 +0100

    Implement blogger update functionality

--- app/repositories/blogger_repository.py
+++ app/repositories/blogger_repository.py
@@ -1,3 +1,6 @@
+from app.models.blogger import Blogger
+from sqlmodel import select
+
 
 class BloggerRepository:
 
@@ -5,6 +8,28 @@ class BloggerRepository:
         self.session = session
 
     def save(self, blogger):
+        self.session.add(blogger)
+        self.session.commit()
+        self.session.refresh(blogger)
+
+        return blogger
+
+    def find_by_id(self, blogger_id):
+        return self.session.get(Blogger, blogger_id)
+
+    def find_by_username(self, username):
+        statement = select(Blogger).where(Blogger.username == username)
+        return self.session.exec(statement).first()
+
+    def find_by_email(self, email):
+        statement = select(Blogger).where(Blogger.email == email)
+        return self.session.exec(statement).first()
+
+    def find_all(self):
+        statement = select(Blogger)
+        return self.session.exec(statement).all()
+
+    def update(self, blogger):
         self.session.add(blogger)
         self.session.commit()
         self.session.refresh(blogger)
--- test/repositories/test_blogger_repository.py
+++ test/repositories/test_blogger_repository.py
@@ -1,41 +1,5 @@
-# import pytest
-#
-# from models.blogger import Blogger
-# from models.user_role import Role
-# from sqlmodel import SQLModel, Session, create_engine
-#
-#
-# engine = create_engine(
-#     "sqlite://",
-#     connect_args={"check_same_thread": False},
-# )
-#
-#
-# class TestBloggerRepository:
-#
-#     @pytest.fixture
-#     def session(self):
-#         SQLModel.metadata.create_all(engine)
-#
-#         with Session(engine) as session:
-#             yield session
-#
-#         SQLModel.metadata.drop_all(engine)
-#
-#     def test_create_blogger(self, session):
-#
-#         blogger = Blogger(
-#             username="colly",
-#             email="colly@gmail.com",
-#             password="meg123",
-#         )
-#
-#         assert blogger.username == "colly"
-#         assert blogger.email == "colly@gmail.com"
-#         assert blogger.password == "meg123"
-#         assert blogger.role == Role.BLOGGER
-import pytest
-
+import email
+from uuid import uuid4
 from app.models.blogger import Blogger
 from app.repositories.blogger_repository import BloggerRepository
 
@@ -48,9 +12,130 @@ def test_that_i_save_blogger(db_session):
         email="collete@gmail.com",
         password="123456"
     )
-
     saved_blogger = repository.save(blogger)
 
     assert saved_blogger.id == blogger.id
     assert saved_blogger.username == "collete"
     assert saved_blogger.email == "collete@gmail.com"
+
+def test_that_I_find_blogger_by_id(db_session):
+    repository = BloggerRepository(db_session)
+    blogger = Blogger(
+        username="collete",
+        email="colly@gmail.com",
+        password ="123456"
+    )
+    repository.save(blogger)
+    found_blogger = repository.find_by_id(blogger.id)
+
+    assert found_blogger is not None
+    assert found_blogger.id == blogger.id
+    assert found_blogger.username == "collete"
+    assert found_blogger.email == "colly@gmail.com"
+
+def test_that_I_return_none_when_blogger_does_not_exist(db_session):
+    repository = BloggerRepository(db_session)
+    blogger_id = uuid4()
+    found_blogger = repository.find_by_id(blogger_id)
+    assert found_blogger is None
+
+def test_that_I_find_blogger_by_username(db_session):
+    repository = BloggerRepository(db_session)
+
+    blogger = Blogger(
+        username="collete",
+        email = "colly@gmail.com",
+        password = "wes123"
+    )
+
+    repository.save(blogger)
+    found_blogger = repository.find_by_username("collete")
+
+    assert found_blogger is not None
+    assert found_blogger.id == blogger.id
+    assert found_blogger.username == "collete"
+    assert found_blogger.email == "colly@gmail.com"
+
+def test_that_I_return_none_when_username_does_not_exist(db_session):
+    repository = BloggerRepository(db_session)
+    found_blogger = repository.find_by_username("User not found")
+
+    assert found_blogger is None
+
+def test_that_I_find_blogger_by_email(db_session):
+    repository = BloggerRepository(db_session)
+    blogger = Blogger(
+        username="collete",
+        email="collete@gmail.com",
+        password="123456"
+    )
+    repository.save(blogger)
+    found_blogger = repository.find_by_email("collete@gmail.com")
+    assert found_blogger is not None
+    assert found_blogger.id == blogger.id
+    assert found_blogger.username == "collete"
+    assert found_blogger.email == "collete@gmail.com"
+
+def test_that_I_return_none_when_email_does_not_exist(db_session):
+    repository = BloggerRepository(db_session)
+    found_blogger = repository.find_by_email("unknown@gmail.com")
+    assert found_blogger is None
+
+def test_that_I_find_all_bloggers(db_session):
+    repository = BloggerRepository(db_session)
+    blogger_one = Blogger(
+        username="collete",
+        email="collete@gmail.com",
+        password="123456"
+    )
+    blogger_two = Blogger(
+        username="john",
+        email="john@gmail.com",
+        password="password123"
+    )
+    repository.save(blogger_one)
+    repository.save(blogger_two)
+    bloggers = repository.find_all()
+    assert len(bloggers) == 2
+    assert bloggers[0].username == "collete"
+    assert bloggers[1].username == "john"
+
+def test_that_find_all_returns_empty_list_when_no_bloggers_exist(db_session):
+    repository = BloggerRepository(db_session)
+    bloggers = repository.find_all()
+    assert bloggers == []
+
+def test_that_I_update_a_blogger(db_session):
+    repository = BloggerRepository(db_session)
+    blogger = Blogger(
+        username="collete",
+        email="collete@gmail.com",
+        password="123456"
+    )
+    repository.save(blogger)
+    blogger.username = "colletecolly"
+    blogger.email = "colly@gmail.com"
+    updated_blogger = repository.update(blogger)
+
+    assert updated_blogger.username == "colletecolly"
+    assert updated_blogger.email == "colly@gmail.com"
+
+def test_that_updated_blogger_is_persisted(db_session):
+    repository = BloggerRepository(db_session)
+
+    blogger = Blogger(
+        username="collete",
+        email="collete@gmail.com",
+        password="123456"
+    )
+
+    repository.save(blogger)
+    blogger.username = "new_username"
+    blogger.email = "new@gmail.com"
+
+    repository.update(blogger)
+
+    found_blogger = repository.find_by_id(blogger.id)
+
+    assert found_blogger.username == "new_username"
+    assert found_blogger.email == "new@gmail.com"
